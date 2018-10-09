0ecf2cdd3703380787092d377f754780e9c9b31f
Vu Quang Thai <vuquangthai2201@gmail.com>
2018-10-09 10:41:28 +0700

    finish chapter3

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+test "should get root" do
+  get root_url
+  assert_response :success
+end
+
+test "should get home" do
+  get static_pages_home_url
+  assert_response :success
+end
+
+test "should get help" do
+  get static_pages_help_url
+  assert_response :success
+end
+
+test "should get about" do
+  get static_pages_about_url
+  assert_response :success
+end
